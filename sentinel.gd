89a930cf04e0f15ff66e1a53c875d195cf05bd1a
DrOtter3000 <info@darknet-online.com>
2024-10-04 18:47:40 +0200

    add attack timer to sentinel

--- sentinel.gd
+++ sentinel.gd
@@ -1,1 +1,16 @@
 extends Node3D
+
+@onready var timer: Timer = $Timer
+
+@export var fire_rate := 6.0
+
+
+func _ready() -> void:
+	timer.wait_time = 60 / fire_rate
+	print(timer.wait_time)
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
+	print(str(name) + str(" firing"))
+	timer.start()
