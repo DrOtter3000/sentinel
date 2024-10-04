27333fb279b8cebf0c244bf988c8c35df3ff5b16
DrOtter3000 <info@darknet-online.com>
2024-10-04 19:51:09 +0200

    make buying sentinels work

--- player.gd
+++ player.gd
@@ -7,6 +7,7 @@ extends Node3D
 @onready var lbl_money_value: Label = %LblMoneyValue
 
 @export var money := 100
+@export var price_per_sentinel := 12
 @export var construction_mode := true
 @export var sentinel_scene: PackedScene
 
@@ -17,12 +18,12 @@ func _process(delta: float) -> void:
 	if construction_mode:			
 		
 		if sentinel_ready_to_build:
-			lbl_status.text = "Add Villager"
-		else:
-			lbl_status.text = ""
+			view_message("Add Villager")
+
 		
 		if Input.is_action_just_pressed("RMB"):
 			sentinel_ready_to_build = false
+			view_message("")
 		
 		var mouse_position: Vector2 = get_viewport().get_mouse_position()
 		construction_ray_cast.target_position = $Camera3D.project_local_ray_normal(mouse_position) * 1000.0
@@ -40,13 +41,19 @@ func _process(delta: float) -> void:
 					var sentinel = sentinel_scene.instantiate()
 					get_node("../Sentinels").add_child(sentinel)
 					sentinel.global_position = construction_ray_cast.get_collision_point()
+					money -= price_per_sentinel
+					update_lbl_money()
+					view_message("")
 					sentinel_ready_to_build = false
 	
 	update_lbl_money()
 
 
 func _on_btn_recruit_pressed() -> void:
-	sentinel_ready_to_build = true
+	if money >= price_per_sentinel:
+		sentinel_ready_to_build = true
+	else:
+		view_message("Insufficient Funds")
 
 
 func update_lbl_money() -> void:
@@ -55,3 +62,8 @@ func update_lbl_money() -> void:
 
 func update_lbl_health(amount: int, maximum: int) -> void:
 	lbl_health_value.text = str(amount) + " / " + str(maximum)
+
+
+func view_message(text: String) -> void:
+	lbl_status.text = text
+	
