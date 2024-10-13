5dc8b32fc26d7b43dec684bbc888a7f1634c21b4
DrOtter3000 <info@darknet-online.com>
2024-10-13 18:56:02 +0200

    add musket sound to sentinels

--- Effects/muzzle.gd
+++ Effects/muzzle.gd
@@ -4,10 +4,13 @@ extends Node3D
 @onready var omni_light_3d: OmniLight3D = $OmniLight3D
 @onready var timer: Timer = $Timer
 @onready var gpu_particles_3d: GPUParticles3D = $GPUParticles3D
+@onready var audio_stream_player_3d: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
 
 func _ready() -> void:
 	gpu_particles_3d.emitting = true
+	audio_stream_player_3d.pitch_scale = randf_range(.8, 1.2)
+	audio_stream_player_3d.play()
 
 
 func _process(delta: float) -> void:
